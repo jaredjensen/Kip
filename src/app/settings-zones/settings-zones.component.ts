5b3c17ab70d3288ca9535a866b4e211e71e13f79
godind <godind@hotmail.com>
2022-11-27 18:49:31 -0500

    fix

--- src/app/settings-zones/settings-zones.component.ts
+++ src/app/settings-zones/settings-zones.component.ts
@@ -11,17 +11,66 @@ import { ZonesService, IZonesRegistry } from "../zones.service";
 import { IPathMetaData } from "../app.interfaces";
 import { IZone } from "../app.interfaces";
 
+const treeData: IZonesRegistry[] =
+    [
+      {
+        path: "self.propulsion.port.revolutions",
+        pathDataState: 0,
+        zones: [
+          {
+            "upper": 3500,
+            "lower": 0,
+            "unit": "rpm",
+            "state": 0
+          },
+          {
+            "upper": 5000,
+            "lower": 3500,
+            "unit": "rpm",
+            "state": 1
+          },
+          {
+            "upper": 5500,
+            "lower": 5000,
+            "unit": "rpm",
+            "state": 2
+          },
+        ]
+      },
+      {
+        path: "self.electrical.switches.bank.0.1.state",
+        pathDataState: 2,
+        zones: [
+          {
+            "upper": 5,
+            "lower": 0,
+            "unit": "unitless",
+            "state": 0
+          }
+        ]
+      },
+      {
+        path: "self.electrical.chargers.victron.yieldYesterday",
+        pathDataState: 4,
+        zones: [
+          {
+          "upper": 50,
+          "lower": 0,
+          "unit": "unitless",
+          "state": 0
+          }
+        ]
+      }
+    ];
+
 @Component({
   selector: 'app-settings-zones',
   templateUrl: './settings-zones.component.html',
   styleUrls: ['./settings-zones.component.css']
 })
 export class SettingsZonesComponent implements OnInit, AfterViewInit {
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-
-  tableData = new MatTableDataSource([]);
 
   //displayedColumns: string[] = ['path', 'unit', 'lower', 'upper', 'state', "actions"];
   displayedColumns: string[] = ['path', 'state', "actions"];
@@ -40,67 +89,14 @@ export class SettingsZonesComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.zonesSub = this.appSettingsService.getZonesAsO().subscribe(zones => {
       // this.tableData.data = zones;
-      this.tableData.data =
-        [
-          {
-            path: "self.propulsion.port.revolutions",
-            pathDataState: 0,
-            zones: [
-              {
-                "upper": 3500,
-                "lower": 0,
-                "unit": "rpm",
-                "state": 0
-              },
-              {
-                "upper": 5000,
-                "lower": 3500,
-                "unit": "rpm",
-                "state": 1
-            },
-            {
-                "uuid": "da4c117d-7828-4373-a2c1-d81ddbc6b4a4",
-                "upper": 5500,
-                "lower": 5000,
-                "path": "self.propulsion.port.revolutions",
-                "unit": "rpm",
-                "state": 2
-            },
-            ]
-          },
-          {
-            path: "self.electrical.switches.bank.0.1.state",
-              pathDataState: 2,
-              zones: [
-                {
-                  "upper": 5,
-                  "lower": 0,
-                  "unit": "unitless",
-                  "state": 0
-                }
-              ]
-          },
-          {
-            path: "self.electrical.chargers.victron.yieldYesterday",
-              pathDataState: 4,
-              zones: [
-                {
-                "upper": 50,
-                "lower": 0,
-                "unit": "unitless",
-                "state": 0
-                }
-              ]
-          }
-        ];
     });
   }
 
   ngAfterViewInit() {
-    this.tableData.paginator = this.paginator;
-    this.tableData.sort = this.sort;
-    this.tableData.filter = "";
-    this.cdRef.detectChanges();
+    // this.treeData.paginator = this.paginator;
+    // this.treeData.sort = this.sort;
+    // this.treeData.filter = "";
+    // this.cdRef.detectChanges();
   }
 
   public trackByPath(index: number, item: IZonesRegistry): string {
@@ -108,51 +104,51 @@ export class SettingsZonesComponent implements OnInit, AfterViewInit {
   }
 
   public applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.tableData.filter = filterValue.trim().toLowerCase();
+    // const filterValue = (event.target as HTMLInputElement).value;
+    // this.treeData.filter = filterValue.trim().toLowerCase();
 
-    if (this.tableData.paginator) {
-      this.tableData.paginator.firstPage();
-    }
+    // if (this.treeData.paginator) {
+    //   this.treeData.paginator.firstPage();
+    // }
   }
 
   public openZoneDialog(path?: string): void {
-    let dialogRef;
+    // let dialogRef;
 
-    if (path) {
-      const thisZone: IZonesRegistry = this.tableData.data.find((zone: IZonesRegistry) => {
-        return zone.path === path;
-        });
+    // if (path) {
+    //   const thisZone: IZonesRegistry = this.treeData.data.find((zone: IZonesRegistry) => {
+    //     return zone.path === path;
+    //     });
 
-      if (thisZone) {
-        dialogRef = this.dialog.open(DialogEditZone, {
-          data: thisZone
-        });
-      }
-    } else {
-        dialogRef = this.dialog.open(DialogNewZone, {
-        });
-    }
+    //   if (thisZone) {
+    //     dialogRef = this.dialog.open(DialogEditZone, {
+    //       data: thisZone
+    //     });
+    //   }
+    // } else {
+    //     dialogRef = this.dialog.open(DialogNewZone, {
+    //     });
+    // }
 
-    dialogRef.afterClosed().subscribe((zone: IZone) => {
-      if (zone === undefined || !zone) {
-        return; //clicked Cancel, click outside the dialog, or navigated await from page using url bar.
-      } else {
-        // TODO: FIX
-        // if (zone.uuid) {
-        //   this.editZone(zone);
-        // } else {
-        //   zone.uuid = this.newUuid();
-        //   this.addZone(zone);
-        // }
-      }
-    });
+    // dialogRef.afterClosed().subscribe((zone: IZone) => {
+    //   if (zone === undefined || !zone) {
+    //     return; //clicked Cancel, click outside the dialog, or navigated await from page using url bar.
+    //   } else {
+    //     // TODO: FIX
+    //     // if (zone.uuid) {
+    //     //   this.editZone(zone);
+    //     // } else {
+    //     //   zone.uuid = this.newUuid();
+    //     //   this.addZone(zone);
+    //     // }
+    //   }
+    // });
   }
 
   public addZone(zone: IZone) {
-    let zones: IZone[] = this.appSettingsService.getZones();
-    zones.push(zone);
-    this.appSettingsService.saveZones(zones);
+    // let zones: IZone[] = this.appSettingsService.getZones();
+    // zones.push(zone);
+    // this.appSettingsService.saveZones(zones);
   }
 
   public editZone(zone: IZone) {
