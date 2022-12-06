2c1e15defd6af8e04b63faa85fcfe30c456cc838
godind <godind@hotmail.com>
2022-12-06 14:17:39 -0500

    new meta config tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -95,6 +95,7 @@ import { DataBrowserRowComponent, DialogUnitSelect } from './data-browser-row/da
 import { ModalUserCredentialComponent } from './modal-user-credential/modal-user-credential.component';
 import { WidgetRaceTimerComponent } from './widget-race-timer/widget-race-timer.component';
 import { WidgetLoginComponent } from './widget-login/widget-login.component';
+import { SettingsMetaComponent } from './settings-meta/settings-meta.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/page/0', pathMatch: 'full' },
@@ -172,6 +173,7 @@ const appNetworkInitializerFn = (appNetInitSvc: AppNetworkInitService) => {
     WidgetRaceTimerComponent,
     ModalUserCredentialComponent,
     WidgetLoginComponent,
+    SettingsMetaComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/settings-meta/settings-meta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-meta',
+  templateUrl: './settings-meta.component.html',
+  styleUrls: ['./settings-meta.component.css']
+})
+export class SettingsMetaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
